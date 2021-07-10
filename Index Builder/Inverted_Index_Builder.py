6f073cbd029cedf7065f129a4bb1ce7c350f4b8a
Charbel Chucri <charbel11chucri@gmail.com>
2021-07-10 22:19:50 +0300

    Added biword inverted index

---
+++ Index Builder/Inverted_Index_Builder.py
@@ -0,0 +1,107 @@
+import Preprocessor
+import os
+import re
+
+directoryPath = 'C:\Website-Collections\Competitive Programming Tutorials'
+
+#Returns an array containing the paths of all HTML files in the cur directory
+def getAllHTMLFiles(curDir):
+    paths = []
+    for curPath, folders, files in os.walk(curDir):
+        for file in files:
+            if (re.search(r".html$", file)):
+                paths.append(os.path.join(curPath, file))
+    return paths
+
+#Generates the text file that contains the docs in order
+def generateDocMap(paths):
+    f = open("Document Order.txt", "w", encoding="utf-8")
+    for path in paths:
+        f.write(path + '\n')
+    f.close()
+
+#Generates the pairs (term, docID) for all documents given their paths
+def generateTermsDocIDMap(paths):
+    termsToDoc = []
+    docID = 0
+    for path in paths:
+        tokens = Preprocessor.getTokensFromHTMLDoc(path)
+        for token in tokens:
+            termsToDoc.append((token, docID))
+        docID += 1
+    return termsToDoc
+
+
+#Generates the pairs ((term0, term1), docID) for all documents given their paths
+def generateBiwordsDocIDMap(paths):
+    termsToDoc = []
+    docID = 0
+    for path in paths:
+        tokens = Preprocessor.getTokensFromHTMLDoc(path)
+        for i in range(len(tokens)-1):
+            termsToDoc.append(((tokens[i], tokens[i+1]), docID))
+        docID += 1
+    return termsToDoc
+
+#Given a dictionary term -> docIDs, we store it in a text file as
+#term: freqID ID1, ID2, ..., IDn
+def storePostingLists(postingLists):
+    f = open("Inverted Indexes.txt", "w", encoding="utf-8")
+    for post in postingLists:
+        f.write(post + ": ")
+        f.write(str(len(postingLists[post])) + " ")
+        f.write(", ".join([str(i) for i in postingLists[post]]))
+        f.write('\n')
+    f.close()
+
+#Given a biword -> docIDs, we store it in a text file as
+#term1, term2: freqID ID1, ID2, ..., IDn
+def storeBiwordPostingLists(postingLists):
+    f = open("Biword Inverted Indexes.txt", "w", encoding="utf-8")
+    for post in postingLists:
+        pair = ", ".join([post[0], post[1]])
+        f.write(pair + ": ")
+        f.write(str(len(postingLists[post])) + " ")
+        f.write(", ".join([str(i) for i in postingLists[post]]))
+        f.write('\n')
+    f.close()
+
+def generateBiwordInvertedIndex():
+    paths = getAllHTMLFiles(directoryPath)
+    generateDocMap(paths)
+
+    termsToDoc = generateBiwordsDocIDMap(paths)
+    termsToDoc = list(dict.fromkeys(termsToDoc))
+    termsToDoc.sort()
+    
+    postingLists = {}
+    for pair in termsToDoc:     #pair contains ((w0, w1), docID) since termsToDoc is an array, not a dict
+        w1 = pair[0][0]
+        w2 = pair[0][1]
+        if (w1[0] < 'a' or w1[0] > 'z' or w2[0] < 'a' or w2[0] > 'z'):
+            continue
+        
+        if pair[0] not in postingLists:
+            postingLists[pair[0]] = []
+        postingLists[pair[0]].append(pair[1])
+
+    storeBiwordPostingLists(postingLists)
+
+def generateInvertedIndex():
+    paths = getAllHTMLFiles(directoryPath)
+    generateDocMap(paths)
+
+    termsToDoc = generateTermsDocIDMap(paths)
+    termsToDoc = list(dict.fromkeys(termsToDoc))
+    termsToDoc.sort()
+
+    postingLists = {}
+    for pair in termsToDoc:
+        if (pair[0][0] < 'a' or pair[0][0] > 'z'):
+            continue
+
+        if pair[0] not in postingLists:
+            postingLists[pair[0]] = []
+        postingLists[pair[0]].append(pair[1])
+
+    storePostingLists(postingLists)
---
+++ Index Builder/Preprocessor.py
@@ -0,0 +1,57 @@
+from io import StringIO
+from html.parser import HTMLParser
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+from nltk.tokenize import word_tokenize
+
+#nltk.download('punkt')
+#nltk.download('stopwords')
+
+stopW = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+patternScript = r'<[ ]*script.*?\/[ ]*script[ ]*>'
+patternStyle = r'<[ ]*style.*?\/[ ]*style[ ]*>'
+
+# HTML Stripping code; It removes all HTML related text from an HTML file
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+def stripTags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+#Normalizes the given strings and returns its corresponding tokens
+def normalize(curData):
+    curData = curData.lower()  # Everything becomes lowercase
+    curData = re.sub('[^\w\s]+', ' ', curData)  # Remove punctuation
+    initialTokens = word_tokenize(curData)  # Tokenize
+    tokens = [stemmer.stem(word) for word in initialTokens if
+              len(word) > 1 and not word in stopW]  # Remove Stop Words & stem the token
+
+    return tokens
+
+#Returns a tokenized version of the HTML document present at path
+def getTokensFromHTMLDoc(path):
+    curFile = open(path, "r", encoding="utf-8")
+    curData = curFile.read()
+    curFile.close()
+
+    # (REMOVE <SCRIPT> to </script> and variations)
+    curData = re.sub(patternScript, ' ', curData, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+    # (REMOVE HTML <STYLE> to </style> and variations)
+    curData = re.sub(patternStyle, ' ', curData, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+#    curData = html2text.html2text(curData)
+    curData = stripTags(curData)  # Remove HTML data
+
+    return normalize(curData)
